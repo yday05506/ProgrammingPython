97a976e916827101a87cef288292603a299d7a8a
yday <yangdayeon2@gmail.com>
2022-03-22 10:13:57 +0900

    변수 선언, 초기화, 여러 가지 출력하는 방법

--- 20220322_helloworld.py
+++ 20220322_helloworld.py
@@ -1,3 +1,7 @@
 print("Hello, world!")  #큰따옴표 == 작은따옴표(구분 없음)
-print('Hello, 양다연!')
+name = '양다연'
+print('Hello, ' +name+ '!') #java 형식
+print('hello, %s' % name)   #C 형식
+print('hello, {}!'.format(name))    #format 함수
+print(f'hello, {name}!')    #f-string 형식
 print("Redvelvet's favorite song is Red Flavour.")
