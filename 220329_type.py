aef737adbf8d3ab62a78aa00161c0a169b6d245f
yday <yangdayeon2@gmail.com>
2022-04-05 09:50:23 +0900

    str(), int(), float(), +연산자, *연산자

--- 220329_type.py
+++ 220329_type.py
@@ -24,3 +24,20 @@ print(27%10)   #python : 나머지 = 7
 print(type(10/27))   #<class 'float'>
 print(type(10//27))   #<class 'int'>
 print(10/5) #2.0 <class 'float'> -> 나누기를 하면 무조건 float
+
+#변수 이름 규칙, 자료형 변환
+#my_mbti, my_function(), MyClass #myMbti:camel-case, my_mbti:snake_case #변수, 함수:snake-case, 클래스:camel-case
+my_mbti = 'INFP'
+print(f'My Mbti : {my_mbti}')
+print('My MBTI : ' + my_mbti + ', age : ' + str(age))  #java:String.toString(age); python:str(age)
+height = '161'
+print(int(height) + 10) #java: Float.parseFloat(height); python: float(height)
+#str(), int(), float() : 자료형 변환 함수
+#+ 연산자 : 숫자+숫자 -> 덧셈, 문자+문자 -> 문자문자
+#* 연산자 : 숫자+숫자 -> 곱셈, 문자*숫자 -> 문자를 숫자만큼 반복
+print(18 + 2)   #20
+print('18'+'2') #'182'
+print(18 * 2) #36
+print('2' * 4) #'2222'
+#짝을 10번 출력
+print('짝' * 10)
