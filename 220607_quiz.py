92014d03cc01769c1fa4c4723e55921e49dfea18
yday <yangdayeon2@gmail.com>
2022-06-07 10:18:12 +0900

    Quiz1. 전화번호 -, /, 띄어쓰기 삭제 Quiz2. 학번으로 학년, 반, 학과, 번호 출력 Quiz3. 자리 수의 숫자를 다 더하기 Quiz4. 369 게임

--- 220607_quiz.py
+++ 220607_quiz.py
@@ -6,6 +6,7 @@ print('-' * 20)
 
 # 2. 학번 -> 학년, 반. 학과, 번호 출력하기
 student_number = '2108'
+
 # >> 2학년 1반 뉴미디어소프트웨어과 08번
 if (student_number[1] == '1') | (student_number[1] == '2'):
     major = '뉴미디어소프트웨어과'
@@ -14,8 +15,10 @@ elif (student_number[1] == '3') | (student_number[1] == '4'):
 elif (student_number[1] == '5') | (student_number[1] == '6'):
     major = '뉴미디어디자인과'
 print(f'{student_number[0]}학년 {student_number[1]}반 {major} {student_number[2:]}번')
+
 # >> 2학년 1반 뉴미디어소프트웨어과 8번
 print(f'{student_number[0]}학년 {student_number[1]}반 {major} {int(student_number[2:])}번')
+
 # >> if문 안 쓰고
 majors = ['뉴미디어소프트웨어과', '뉴미디어소프트웨어과', '뉴미디어웹솔루션과','뉴미디어웹솔루션과', '뉴미디어디자인과', '뉴미디어디자인과']
 index = int(student_number[1])
@@ -31,6 +34,7 @@ n2 = int(number % 100 / 10)
 n3 = int(number % 10)
 print(n1 + n2 + n3)
 # >> 7
+
 number = 523523
 sum_val = 0
 while True: #while number != 0
@@ -39,7 +43,24 @@ while True: #while number != 0
     sum_val += number % 10
     number = number // 10
 print(sum_val)
+
+#문자 한 자리씩 빼서 계산
+number = 523523
+number_s = str(number)  #523523
+sum_val2 = 0
+for n in number_s:
+    sum_val2 += int(n)
+print(sum_val2)
 # >> 20
+# -> 이게 더 파이썬 같은 코드
+
+sum_val3 = 0
+for index in range(len(number_s)):
+    sum_val3 += int(number_s[index])
+print(sum_val3)
+# >> 20
+# -> 자바 같은 코드
+
 # 나머지 = number % 10   # 3
 # number = number // 10   #523523 -> 52352
 # 나머지 = number % 10   # 2
