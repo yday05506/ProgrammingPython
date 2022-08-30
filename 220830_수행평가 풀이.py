11e9f727cdedd943aeb1f561105b58b01600d740
yday <yangdayeon2@gmail.com>
2022-08-30 10:18:44 +0900

    수행평가 1-2

--- 220830_수행평가 풀이.py
+++ 220830_수행평가 풀이.py
@@ -17,3 +17,21 @@ def sum_odd2(phone_number):
     return sum([int(number) for number in phone_number if int(number) % 2 != 0])
 result = sum_odd2('01088792517')
 print(result)
+print('----------')
+
+
+# 2. 영단어를 인자로 받아, 모음인 a, e, i, o, u만 *로 대체하여 리턴하는 encrypt 함수를 작성하시오.
+def encrypt(word):
+    result = ''
+    for char in word:
+        # 모음인지 구별하기
+        if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
+            # 별로 바꾸기
+            char = '*'
+            result += char
+        # 다르면
+        else:
+            # 그대로 쓰기
+            result += char
+    return result
+print(encrypt('ive'))
