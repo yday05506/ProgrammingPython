eb0f6546506b4f2f070d9c4dd5c8aa87bc37edd1
yday <yangdayeon2@gmail.com>
2022-09-20 08:50:57 +0900

    수행평가 1-5

--- 220830_수행평가 풀이.py
+++ 220830_수행평가 풀이.py
@@ -86,4 +86,20 @@ def abbreviate2(name):
 # print(abbreviate2('Maverick'))   # M.
 # print(abbreviate2('HAE CHAN'))   # H. C.
 print(abbreviate2('jin young park'))   # J. Y. P.
+print('----------')
 
+
+# 5. 사용한 시간(분)을 인자로 받아 PC방 요금을 리턴
+def fare_pc(minutes):
+    # minutes을 10으로 나누자. 몫
+    share = minutes // 10
+    # 몫 * 1000 = 요금
+    fare = share * 1000
+    # minutes를 10으로 나눈 나머지가 있으면, +1000
+    if minutes % 10 != 0 :
+        fare += 1000
+    return fare
+print(fare_pc(10))
+print(fare_pc(30))
+print(fare_pc(60))
+print(fare_pc(62))
