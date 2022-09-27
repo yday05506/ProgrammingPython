449382bf79b8266fca9b7e1774bab44916050ee5
yday <yangdayeon2@gmail.com>
2022-09-27 09:50:31 +0900

    클래스(채영, 소리)

--- 220927_채영소리.py
+++ 220927_채영소리.py
@@ -6,15 +6,24 @@
 '''
 사람 - 이름, 나이, 성별
 선생님 - 과목, 담임반
+
+클래스는 웬만하면 단수
+단 여러 개 담으면 복수
+
+수치가 들어오는 값이면, 가능하면 숫자로 세팅
+나이, 생년 등은 숫자, 핸드폰 번호는 문자로 처리
+01012345678dms 10억 1234만 5678이 아님 -> '01012345678'
+
+더 이상 내 코드는 나만 보지 않는다. 누구나 내 코드를 읽을 수 있도록 한다
 '''
 
-class People:
+class Person:
     def __init__(self, name):
         self.name = name
         self.age = None
         self.gender = None
 
-    def set_age(self, age):#매개변수와 함수이름을 같게하지말것
+    def set_age(self, age):#매개변수와 함수이름을 같게 하지 말 것
         self.age = age
 
     def set_gender(self, gender):
@@ -25,12 +34,12 @@ class People:
         return f'{self.name}\t{self.age}\t{self.gender}'
 
 # people에 값넣기
-고나근샘 = People('고낙은')
+고나근샘 = Person('고낙은')
 고나근샘.set_age('34')
 고나근샘.set_gender('남자')
 print(고나근샘)
 
-class Teacher(People):
+class Teacher(Person):
     def subject(self, sub):
         self.sub = sub
     def HRT(self, hrt):
