ec469432c7e6305eed8fec46070984c3e5f1db03
yday <yangdayeon2@gmail.com>
2022-10-18 10:20:20 +0900

    start Drink

---
+++ Amasvin/drink.py
@@ -0,0 +1,15 @@
+# Drink
+    # name
+    # price
+    # cup_size
+    # sugar : 당도
+    # ice
+# Drink <- Coffee
+# Drink <- Bubbletea
+    # pearl
+
+# 옵션들
+# 컵사이즈 : 기본, 점보
+# 펄 : 타피오카, 알로에, 곤약, 코코넛
+# 얼음량 : 적게, 기본, 많이, 없음
+# 당도 : 30%, 50%, 70%, 100%
