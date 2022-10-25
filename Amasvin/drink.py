123f33cf615909e033f4beed7d2c8bce278aeb6b
yday <yangdayeon2@gmail.com>
2022-10-25 10:10:51 +0900

    Drink 값 클래스변수[인덱스]로 저장, set_cup_size()

--- Amasvin/drink.py
+++ Amasvin/drink.py
@@ -23,11 +23,27 @@ class Drink :
     def __init__(self, name, price):
         self.name = name
         self.price = price
-        self.cup_size = '레귤러'
-        self.sugar = '50%'
-        self.ice = '기본'
+        self.cup_size = 0   #0:레귤러, 1:점보
+        self.sugar = 1  #0:30%, 1:50%, 2:70%, 3:100%
+        self.ice = 2    #0:없음, 1:적게, 2:기본, 3:많게
     def __str__(self):
-        return f'이름 : {self.name}\t가격 : {self.price}원\t 컵 사이즈 : {self.cup_size}\t 당도 : {self.sugar}\t 얼음량 : {self.ice}'
+        return f'이름 : {self.name}\t가격 : {self.price}원\t 컵 사이즈 : {Drink._CUP_SIZES[self.cup_size]}\t 당도 : {Drink._SUGARS[self.sugar]}\t 얼음량 : {Drink._ICES[self.ice]}'
+
+    def set_cup_size(self):
+        # 사용자에게 숫자 묻기 → 1:레귤러, 2:점보
+        for index, cup_size_label in enumerate(Drink._CUP_SIZES):
+            print(f'{index+1}.{cup_size_label}')
+        cup_size = input('컵사이즈를 선택하세요: ')   # input으로 넣으면 숫자를 넣더라도 문자열로 받아들임
+        if cup_size == '':  # 엔터 치면 기본값
+            self.cup_size = 0
+        else :
+            self.cup_size = int(cup_size)-1 # 숫자를 제대로 입력했을 때
+        #self.cup_size가 점보(2)일 때, 가격 +500원
+        if self.cup_size == 1:
+            self.price += 500
+
+
 
 음료1 = Drink("아메리카노", 1800)
+음료1.set_cup_size()
 print(음료1)
