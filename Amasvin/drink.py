44811749c28c497d92c301f24f440582a43f0e9f
yday <yangdayeon2@gmail.com>
2022-11-01 09:09:05 +0900

    Drink set_sugar(), set_ice(), order

--- Amasvin/drink.py
+++ Amasvin/drink.py
@@ -24,8 +24,8 @@ class Drink :
         self.name = name
         self.price = price
         self.cup_size = 0   #0:레귤러, 1:점보
-        self.sugar = 1  #0:30%, 1:50%, 2:70%, 3:100%
-        self.ice = 2    #0:없음, 1:적게, 2:기본, 3:많게
+        self.sugar = 1 #0:없음, 1:적게, 2:기본, 3:많게
+        self.ice = 2    #0:30%, 1:50%, 2:70%, 3:100%
     def __str__(self):
         return f'이름 : {self.name}\t가격 : {self.price}원\t 컵 사이즈 : {Drink._CUP_SIZES[self.cup_size]}\t 당도 : {Drink._SUGARS[self.sugar]}\t 얼음량 : {Drink._ICES[self.ice]}'
 
@@ -43,17 +43,34 @@ class Drink :
             self.price += 500
 
     def set_sugar(self):
+        # 옵션 보여주기 1. 30%, 2. 50%...
         for index, sugar_label in enumerate(Drink._SUGARS):
             print(f'{index+1}.{sugar_label}')
+        # 사용자 입력 받기
         sugar = input('당도를 선택하세요: ')
+        # 그냥 엔터만, 기본값 : 1
         if sugar == '':
             self.sugar = 1
+        # 숫자 입력하면, -1 -> index 바꿔주기
         else:
             self.sugar = int(sugar)-1
+    def set_ice(self):
+        for index, ice_label in enumerate(Drink._ICES):
+            print(f'{index+1}.{ice_label}')
+        ice = input('얼음량을 선택하세요: ')
+        if ice == '':
+            self.ice = 2
+        else :
+            self.ice = int(ice)-1
 
-
+    def order(self):
+        self.set_cup_size()
+        self.set_sugar()
+        self.set_ice()
 
 음료1 = Drink("아메리카노", 1800)
-음료1.set_cup_size()
-음료1.set_sugar()
+# 음료1.set_cup_size()
+# 음료1.set_sugar()
+# 음료1.set_ice()
+음료1.order()
 print(음료1)
