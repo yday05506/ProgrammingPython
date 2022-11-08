7d12b8c62ca87b991632b471c95f3a0231f0f6d3
yday <yangdayeon2@gmail.com>
2022-11-08 10:21:04 +0900

    start order

---
+++ Amasvin/order.py
@@ -0,0 +1,22 @@
+class Order:
+    def __init__(self):
+        self.menu = []  # 메뉴판
+        self.init_menu()    # 메뉴판 초기화
+        self.order_menu = []    # 주문한 음료수 리스트
+    def __str__(self):
+        pass
+    
+    # 메뉴판 초기화
+    def init_menu(self):
+        pass
+    
+    def order(self):
+        #반복
+            self.show_menu()  # 메뉴판 보여주기
+            # 음료수 고르기(사용자 입력 받기)
+            # 새로운 음료수로 생성, 옵션들 주문
+            # 주문한 음료수 리스트에 새로운 음료수 추가
+        # 주문한 음료수 리스트 출력
+        
+    def show_menu(self):
+        pass
