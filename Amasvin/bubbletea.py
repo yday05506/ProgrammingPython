421fb3984509a55c8ec7b8cfb6a4e71a9a506753
yday <yangdayeon2@gmail.com>
2022-11-15 08:47:52 +0900

    ***** if __name__ == '__main__':

--- Amasvin/bubbletea.py
+++ Amasvin/bubbletea.py
@@ -31,7 +31,7 @@ class Bubbletea(Drink):
         # 내 set_pearl() 호출
         self.set_pearl()
 
-
-버블티1 = Bubbletea('타로버블티', 3700)
-버블티1.order()
-print(버블티1)
+if __name__ == '__main__':  # *****
+    버블티1 = Bubbletea('타로버블티', 3700)
+    버블티1.order()
+    print(버블티1)
