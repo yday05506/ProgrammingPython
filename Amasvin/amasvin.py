195165312a1121c1b12eea7db6f5fde55e93fd4b
yday05506 <yangdayeon2@gmail.com>
2022-11-22 09:09:19 +0900

    order 주문 내역 출력, 가격 총합계 출력, amasvin에서 실행

--- Amasvin/amasvin.py
+++ Amasvin/amasvin.py
@@ -1,1 +1,5 @@
 # 아마스빈 메뉴 보고 주문하는 콘솔 프로그램
+from order import Order #amasivn.order로 나오는데 실제로는 같은 폴더에 있으므로 order로 써도 됨
+
+order = Order()
+order.order()
--- Amasvin/order.py
+++ Amasvin/order.py
@@ -11,12 +11,15 @@ class Order:
     def __str__(self):
         # self.order_menu에서 drnk 하나씩 꺼내서 출력
         # 총 주문 금액 출력
-        return '\n'.join(str(drink) for drink in self.order_menu)
+        total_price = 0
+        for drink in self.order_menu:
+            total_price += drink.price
+        return '\n'.join(str(drink) for drink in self.order_menu) + f'\n총 주문 금액은 {total_price}원입니다.'
     
     # 메뉴판 초기화
     def init_menu(self):
         new_menu = BubbleTea("하동녹차오레오", 4500)
-        self.menu.append(new_menu)
+        self.menu.append(new_menu)  # 리스트에 추가 : append()
         new_menu = Coffee("카페 모카", 3000)
         self.menu.append(new_menu)
         new_menu = BubbleTea("라즈베리소다", 2900)
@@ -35,11 +38,14 @@ class Order:
             new_drink = copy(self.menu[int(choice) - 1])
                 # 메뉴 그대로 가져오면, 옵션에 따라 메뉴가 바뀌어 있음
                 # 메뉴는 기본으로 놔두고, 복사해와서 그것의 옵션을 바꾸자
-            new_drink.order()
-            print(new_drink)
+            new_drink.order()   # 음료 주문 받기(옵션)
+            # print(new_drink)
             # 주문한 음료수 리스트에 새로운 음료수 추가
             self.order_menu.append(new_drink)
         # 주문한 음료수 리스트 출력
+        print("-"*80)
+        print("주문하신 음료수는 다음과 같습니다.")
+        print(self) # __str__() 호출
         
     def show_menu(self):
         for index, drink in enumerate(self.menu):
@@ -48,4 +54,3 @@ class Order:
 if __name__ == '__main__':
     order = Order()
     order.order()
-    print(order)
