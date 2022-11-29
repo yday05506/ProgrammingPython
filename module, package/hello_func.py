edbb0703ca36fda3021bdf78702b22dfc0de4c8e
yday05506 <yangdayeon2@gmail.com>
2022-11-29 09:17:49 +0900

    import ~as ~

--- module, package/hello_func.py
+++ module, package/hello_func.py
@@ -2,5 +2,6 @@ def hello():
     print('hello_func 모듈입니다')
     print('hello world')
 
+print('__name__ : ' + name)
 if __name__ == '__main__':
     hello()
--- module, package/insa2.py
+++ module, package/insa2.py
@@ -1,4 +1,4 @@
 import hello2
-import hello_func
+import hello_func as h
 
-hello_funck.hello()
+hello_func.hello()k
